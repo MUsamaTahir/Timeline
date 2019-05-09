33a41c33212f3492aa7cf0bc0cefbd433fc332e7
MUsamaTahir <MUsamaTahir@users.noreply.github.com>
2019-05-09 12:31:50 +0500

    production database changed according to remote db

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ var log = require('../app/lib/logger').child({type: "database"});
 
 module.exports = {
   "development": {
-    "username": "okoysmbdmkaudj",
-    "password": "29fec2e9b2cd11fbccabc94abb546511ef2e4a3340b477bec2f0080e80284395",
-    "database": "d3m7nd4bkj8eh6",
-    "host": "ec2-23-23-92-204.compute-1.amazonaws.com",
+    "username": "postgres",
+    "password": "123",
+    "database": "Timeline",
+    "host": "localhost",
     "dialect": "postgres",
     "logging": function (message) {
       log.debug(message);
