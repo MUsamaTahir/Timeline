d072d808b22f17c7635784191adb5b556b4b4dcc
MUsamaTahir <MUsamaTahir@users.noreply.github.com>
2019-05-09 10:10:58 +0500

    database.js changed a bit

--- config/database.js
+++ config/database.js
@@ -22,10 +22,10 @@ module.exports = {
     }
   },
   "production": {
-    "username": "postgres",
-    "password": "123",
-    "database": "Timeline",
-    "host": "localhost",
+    "username": "okoysmbdmkaudj",
+    "password": "29fec2e9b2cd11fbccabc94abb546511ef2e4a3340b477bec2f0080e80284395",
+    "database": "d3m7nd4bkj8eh6",
+    "host": "ec2-23-23-92-204.compute-1.amazonaws.com",
     "dialect": "postgresql",
     "logging": function (message) {
       log.debug(message);
