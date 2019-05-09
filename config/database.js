05eeac7f78f8de30d266709b82b29f8c105b247b
MUsamaTahir <MUsamaTahir@users.noreply.github.com>
2019-05-09 15:54:23 +0500

    ElephantSQL integrated

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ var log = require('../app/lib/logger').child({type: "database"});
 
 module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "123",
-    "database": "Timeline",
-    "host": "localhost",
-    "dialect": "postgres",
+    "username": "ndjajcbg",
+    "password": "BPG1M3gTidQhqvl9sjRamUbu3qEGDwN2",
+    "database": "ndjajcbg",
+    "dialect": "postgresql",
+    "host": "	isilo.db.elephantsql.com",
     "logging": function (message) {
       log.debug(message);
     }
@@ -22,10 +22,10 @@ module.exports = {
     }
   },
   "production": {
-    "username": "okoysmbdmkaudj",
-    "password": "29fec2e9b2cd11fbccabc94abb546511ef2e4a3340b477bec2f0080e80284395",
-    "database": "d3m7nd4bkj8eh6",
-    "host": "ec2-23-23-92-204.compute-1.amazonaws.com",
+    "username": "ndjajcbg",
+    "password": "BPG1M3gTidQhqvl9sjRamUbu3qEGDwN2",
+    "database": "ndjajcbg",
+    "host": "	isilo.db.elephantsql.com",
     "dialect": "postgresql",
     "logging": function (message) {
       log.debug(message);
