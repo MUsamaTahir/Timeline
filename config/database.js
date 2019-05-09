bf71f2601e2050b880d7d1dda63fdeb4aa9aeaf4
MUsamaTahir <MUsamaTahir@users.noreply.github.com>
2019-05-09 10:09:27 +0500

    Database changes made in database.js

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ var log = require('../app/lib/logger').child({type: "database"});
 
 module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "123",
-    "database": "Timeline",
-    "host": "localhost",
+    "username": "okoysmbdmkaudj",
+    "password": "29fec2e9b2cd11fbccabc94abb546511ef2e4a3340b477bec2f0080e80284395",
+    "database": "d3m7nd4bkj8eh6",
+    "host": "ec2-23-23-92-204.compute-1.amazonaws.com",
     "dialect": "postgres",
     "logging": function (message) {
       log.debug(message);
